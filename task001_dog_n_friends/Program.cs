792cfdac8b4b731a383834e3e5f599330c101385
Max Babich <sapienti@list.ru>
2022-12-15 15:31:12 +0300

    litle edit

--- task001_dog_n_friends/Program.cs
+++ task001_dog_n_friends/Program.cs
@@ -1,15 +1,16 @@
-﻿Console.Write("Enter the speed of the first friend (meters per second):");
-string oper = Console.ReadLine();
-float ffs = float.Parse(oper);  //ffs-fist friend speed
+﻿using System.Globalization;
+ 
+IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+
+
+Console.Write("Enter the speed of the first friend (meters per second):");
+float ffs = float.Parse(Console.ReadLine(), formatter);  //ffs-fist friend speed
 Console.Write("Enter the speed of the second friend (meters per second):");
-string oper1 = Console.ReadLine();
-float sfs = float.Parse(oper1); // sfs-second friend speed
+float sfs = float.Parse(Console.ReadLine(), formatter); // sfs-second friend speed
 Console.Write("Enter the speed of the dog (meters per second):");
-string oper2 = Console.ReadLine();
-float ds = float.Parse(oper2); //ds-dog speed
+float ds = float.Parse(Console.ReadLine(), formatter); //ds-dog speed
 Console.Write("Enter the distance between friends (meters):");
-string oper3 = Console.ReadLine();
-float distance = float.Parse(oper3);
+float distance = float.Parse(Console.ReadLine(), formatter);
 int friend = 2;
 int count = 0;
 float time =0;
